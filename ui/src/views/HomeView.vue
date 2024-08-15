d69a3fc708a870d84a5ac5fb65263cd6c169a078
Arcadell <arcadell.dev@gmail.com>
2024-08-15 20:48:47 +0200

    Added login route

--- ui/src/views/HomeView.vue
+++ ui/src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
   <main>
     <h1>Home route</h1>
+    <RouterLink to="/login">Login</RouterLink>
   </main>
 </template>
