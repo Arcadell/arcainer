ca5cf31081c261fe90ebcb86f64bdc91f5365640
Arcadell <arcadell.dev@gmail.com>
2024-08-15 10:49:27 +0200

    Added user Idetentity

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,7 +1,11 @@
 
 using Api.Routes;
 using Application.Containers.Interfaces;
+using Infrasturcture.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
 
 namespace Api
 {
@@ -13,6 +17,8 @@ namespace Api
 
             // Add services to the container.
             builder.Services.AddAuthorization();
+            builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
 
             builder.Services.AddApiServices();
             builder.Services.AddApplicationServices();
@@ -20,7 +26,17 @@ namespace Api
 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                options.OperationFilter<SecurityRequirementsOperationFilter>();
+            });
 
             var app = builder.Build();
 
@@ -31,12 +47,15 @@ namespace Api
                 app.UseSwaggerUI();
             }
 
+            app.MapGroup("/identity").MapIdentityApi<IdentityUser>();
+
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
 
             app.MapGroup("/container")
                 .MapContainerRoutes()
+                .RequireAuthorization()
                 .WithOpenApi();
 
             app.Run();
--- src/Infrasturcture/Data/ApplicationDbContext.cs
+++ src/Infrasturcture/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -9,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace Infrasturcture.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public DbSet<Container> Containers { get; set; }
 
