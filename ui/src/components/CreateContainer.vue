d1ba544d00f31f77d153b5f4590f52207e506683
Arcadell <arcadell.dev@gmail.com>
2024-10-06 19:24:52 +0200

    Added composerize for converting docker run command

--- ui/src/components/CreateContainer.vue
+++ ui/src/components/CreateContainer.vue
@@ -1,27 +1,30 @@
 <script setup lang="ts">
+// @ts-ignore
+import composerize from 'composerize';
 import { ref } from 'vue';
 import CodeEditor from './CodeEditor.vue';
 
-const composeValue = ref('');
+const dockerRunCommand = ref('');
+const dockerComposeValue = ref('');
 
 const converContainer = () => {
-    composeValue.value = 'Hello'
+    dockerComposeValue.value = composerize(dockerRunCommand.value)
 }
 
 const createContainer = () => {
-    console.log(composeValue.value)
+    console.log(dockerComposeValue.value)
 }
 </script>
 
 <template>
     <div class="main-container-create">
         <div class="generate-docker-compose">
-            <input type="text" placeholder="Docker run command">
+            <input type="text" placeholder="Docker run command" v-model="dockerRunCommand">
             <button class="btn btn-outline" v-on:click="converContainer">Convert</button>
         </div>
 
         <h2>Compose</h2>
-        <CodeEditor :value="composeValue" />
+        <CodeEditor :value="dockerComposeValue" />
         <button v-on:click="createContainer">Create</button>
     </div>
 </template>
